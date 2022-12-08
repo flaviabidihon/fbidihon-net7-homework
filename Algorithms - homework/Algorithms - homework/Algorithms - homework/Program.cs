50b61e953314281a034170576a0d2911648407df
Flavia B <115484938+flaviabidihon@users.noreply.github.com>
2022-12-08 15:58:56 +0200

    Algorithms - homework

---
+++ Algorithms - homework/Algorithms - homework/Algorithms - homework/Program.cs
@@ -0,0 +1,22 @@
+﻿// Bubble sort algorithm
+
+List<int> myList = new List<int> { 1, 9, 5, 6, 3, 2, 7 };
+int temp;
+
+for (int i = 0; i < myList.Count - 1; i++)
+{
+    for (int j = 0; j < myList.Count - 1; j++)
+    {
+        if (myList[j] > myList[j+1])
+        {
+            temp = myList[j + 1];
+            myList[j + 1] = myList[j];
+            myList[j] = temp;
+        }
+    }
+}
+
+foreach (int elem in myList)
+    Console.WriteLine(elem);
+
+ 
