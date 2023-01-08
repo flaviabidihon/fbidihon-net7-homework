9f855c26305c2dadefe64d9f9554e21a3972f667
Flavia B <115484938+flaviabidihon@users.noreply.github.com>
2023-01-08 02:32:13 +0200

    OOP advanced - homework

---
+++ 6. OOP advanced - homework/OOP advanced - homework/OOP advanced - homework/Exercise1/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_advanced___homework.Exercise1
+{
+    public class Transaction
+    {
+        public DateTime _timestamp;
+        public string _type;
+        public decimal _amount;
+
+        public Transaction(string type, decimal amount)
+        {
+            _timestamp = DateTime.Now;
+            _type = type;
+            _amount = amount;
+        }
+    }
+}
---
+++ 6. OOP advanced - homework/OOP advanced - homework/OOP advanced - homework/Exercise3/Shape.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_advanced___homework.Exercise3
+{
+    public abstract class Shape
+    {
+        public virtual string Color { get; set; }
+        public virtual double Area { get; set; } = 0;
+
+        //public Shape(string color, double area)
+        //{
+        //    Color = color;
+        //    Area = area;
+        //}
+
+        public abstract double CalculateArea();
+        public virtual double GetPerimeter()
+        {
+            return 0;
+        }
+        
+    }
+}
