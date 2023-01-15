bc59a28f38c6434e39b3493f06012361d197f886
Flavia B <115484938+flaviabidihon@users.noreply.github.com>
2023-01-16 01:48:22 +0200

    Update Program.cs

--- 7. Exceptions LINQ etc - homework/Exceptions LINQ etc - Homework/Exceptions LINQ etc - Homework/Program.cs
+++ 7. Exceptions LINQ etc - homework/Exceptions LINQ etc - Homework/Exceptions LINQ etc - Homework/Program.cs
@@ -5,21 +5,21 @@ using Exceptions_LINQ_etc___Homework.Exercise4;
 using System.Globalization;
 using System.IO;
 
-//Console.WriteLine(" ------ EXERCISE 1 ------");
-//DateChecker date1 = new DateChecker("14.01.2222");
-//DateChecker date2 = new DateChecker("14.01.1900");      //uncomment line 8 if you want to check the exception
-//date1.RunDateChecker();
-////date2.RunDateChecker();      
+Console.WriteLine(" ------ EXERCISE 1 ------");
+DateChecker date1 = new DateChecker("14.01.2222");
+DateChecker date2 = new DateChecker("14.01.1900");      //uncomment line 8 if you want to check the exception
+date1.RunDateChecker();
+//date2.RunDateChecker();      
 
-//Console.WriteLine();
-//Console.WriteLine(" ------ EXERCISE 2 ------");
-//NameChecker names = new NameChecker();
-//names.ReadAndCheckName();
+Console.WriteLine();
+Console.WriteLine(" ------ EXERCISE 2 ------");
+NameChecker names = new NameChecker();
+names.ReadAndCheckName();
 
-//Console.WriteLine();
-//Console.WriteLine(" ------ EXERCISE 3 ------");
-//SumAndAverage calculator1 = new SumAndAverage();
-//calculator1.Calculator();
+Console.WriteLine();
+Console.WriteLine(" ------ EXERCISE 3 ------");
+SumAndAverage calculator1 = new SumAndAverage();
+calculator1.Calculator();
 
 Console.WriteLine();
 Console.WriteLine(" ------ EXERCISE 3 ------");
